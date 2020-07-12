91fe063dad6999c87b82c9dd7669e423961594b1
Mauro Cicolella <mcicolella@libero.it>
2020-07-12 19:21:52 +0200

    Aggiunto microservice-demo-heroku

---
+++ microservice-demo-heroku/src/test/java/net/emmecilab/microservicedemo/MicroserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.emmecilab.microservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
