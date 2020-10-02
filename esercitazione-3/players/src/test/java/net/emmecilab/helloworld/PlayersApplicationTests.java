a83bf332f72f3a05d45503ba3b1829c9afa4028e
Mauro Cicolella <mcicolella@libero.it>
2020-10-02 14:40:11 +0200

    Aggiornata esercitazione-3 alla v2.3.4 di Spring Boot

--- esercitazione-3/players/src/test/java/net/emmecilab/helloworld/PlayersApplicationTests.java
+++ esercitazione-3/players/src/test/java/net/emmecilab/helloworld/PlayersApplicationTests.java
@@ -1,12 +1,13 @@
 package net.emmecilab.helloworld;
 
+import net.emmecilab.players.PlayersApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = PlayersApplication.class)
 public class PlayersApplicationTests {
 
 	@Test
