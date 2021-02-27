090fd918c228a878d712b99075b5859f6bd26ec3
Mauro Cicolella <mcicolella@libero.it>
2021-02-27 21:42:59 +0100

    Aggiunti dev-tools e jetty-embedded-server

---
+++ dev-tools/src/main/java/net/emmecilab/devtools/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package net.emmecilab.devtools.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Mauro Cicolella
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello()  {
+        return "Hello world from Spring Boot!!";
+    }
+}
