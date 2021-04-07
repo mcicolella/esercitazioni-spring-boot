6d5058462149da263fe93bdf6ece0a8136f03542
Mauro Cicolella <mcicolella@libero.it>
2021-04-07 17:27:25 +0200

    Aggiunto custom-banner

---
+++ custom-banner/src/main/java/net/emmecilab/custom/banner/CustomBannerDemoApplication.java
@@ -0,0 +1,13 @@
+package net.emmecilab.custom.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomBannerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomBannerDemoApplication.class, args);
+    }
+
+}
---
+++ custom-banner/src/test/java/net/emmecilab/custom/banner/CustomBannerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.emmecilab.custom.banner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomBannerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
